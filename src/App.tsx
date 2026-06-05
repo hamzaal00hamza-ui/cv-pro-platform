e1fdff544c186cf8bea63bcef4160856fd27dd39
hamzaal00hamza-ui <hamzaal00hamza@gmail.com>
2026-06-05 22:01:37 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Home from "./pages/Home";
 import CreateCV from "./pages/CreateCV";
 import Payment from "./pages/Payment";
 import Preview from "./pages/Preview";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+import Settings from "./pages/Settings";
 
 export default function App() {
   return (
@@ -11,6 +14,9 @@ export default function App() {
       <Route path="/create-cv" element={<CreateCV />} />
       <Route path="/payment" element={<Payment />} />
       <Route path="/preview" element={<Preview />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/settings" element={<Settings />} />
       <Route path="*" element={<Home />} />
     </Routes>
   );
